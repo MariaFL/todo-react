e9a4a26efe8407300cee189a11fdc32d4ed18134
MariaFL <fedmaria2@gmail.com>
2019-10-05 21:26:18 +0500

    task 17, added first element and styles

---
+++ src/TodoItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TodoItem() {
+  return (
+    <div className="taskItem">
+      <input type="checkbox" className="checkbox"/>
+      <p className="checktext">Todoshechka 1</p>
+    </div>
+  )
+}
+
+export default TodoItem;
