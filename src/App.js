ea78b062b5f859d188413b3c26de9f046e57356b
MariaFL <fedmaria2@gmail.com>
2019-11-10 20:32:29 +0500

    task 23, add separate todo list data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './index.css';
 import TodoItem from './TodoItem';
+import todosData from './todosData';
 
 function App() {
+  const todosList = todosData.map(todo => {
+    return (
+      <TodoItem key={todo.id} todo={todo}/>
+    );
+  })
   return (
     <div className="tasklist">
-      <TodoItem/>
-      <TodoItem/>
-      <TodoItem/>
-      <TodoItem/>
-      <TodoItem/>
+      {todosList}
     </div>
   );
 }
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function TodoItem() {
+function TodoItem(props) {
   return (
     <div className="taskItem">
-      <input type="checkbox" className="checkbox"/>
-      <p className="checktext">Todoshechka 1</p>
+      <input type="checkbox" className="checkbox" checked={props.todo.completed}/>
+      <p className="checktext">{props.todo.text}</p>
     </div>
   )
 }
