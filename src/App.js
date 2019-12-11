07f8f566716a9d0687f7344038c5b5c90c3ab703
MariaFL <fedmaria2@gmail.com>
2019-12-11 11:50:13 +0500

    task 29, stage 4, replace function on class, add state

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import './index.css';
 import TodoItem from './TodoItem';
 import todosData from './todosData';
 
-function App() {
-  const todosList = todosData.map(todo => {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData
+    }
+  };
+  render() {
+    const todosList = todosData.map(todo => {
+      return (
+        <TodoItem key={todo.id} todo={todo}/>
+      );
+    });
     return (
-      <TodoItem key={todo.id} todo={todo}/>
+      <div className="tasklist">
+        {todosList}
+      </div>
     );
-  })
-  return (
-    <div className="tasklist">
-      {todosList}
-    </div>
-  );
+  };
 }
 
 export default App;
